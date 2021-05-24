a4b1dd7086baac754f981f5e47e205326c214753
DavidRMorphew <morphewd@umich.edu>
2021-05-24 17:35:30 -0400

    commit tree e319dd9fc32841ec7cb2e76e8d226fe649f1ef36

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
 // Code Keypad Component Here
+import React, { Component } from 'react'
+class Keypad extends Component {
+    
+    handleKeyUp = (e) => {
+        console.log(`Entering password...`)
+    }
+    
+    render(){
+        return(
+            <div>Keypad
+                <input onKeyUp={this.handleKeyUp} type="password" />
+            </div>
+        )
+    }
+}
+export default Keypad
